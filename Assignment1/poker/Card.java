ec5360012dfe5925f9a642ebbe88049fa70dabac
Tyler Babin <tylerbabin@cmail.carleton.ca>
2015-09-20 20:01:17 -0400

    Refactored royal flush test
    modified the royal flush test slightly.
    Added method to help with checking if cards are in a certain range, will
    be useful for straights and straight flushes.

--- Assignment1/poker/Card.java
+++ Assignment1/poker/Card.java
@@ -1,28 +1,28 @@
 package poker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Card {
-	private String cardValue;
-	private String cardSuit;
+	private String cardValue; //The Card's Face Value
+	private int cardIntValue; //The Card's Face Value as an integer from 1-13, Two being the lowest and Ace being the highest.
+	private String cardSuit; // The Card's Suit
+	private ArrayList<String> validCardVals = new ArrayList<String>(Arrays.asList("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"));
 
 	public Card(String value, String suit) {
 		cardValue = value;
 		cardSuit = suit;
+		cardIntValue = validCardVals.indexOf(cardValue) + 1;
 	}
 
-	public String getCardValue() {
-		return cardValue;
-	}
+	public int getCardIntValue() { return cardIntValue; }
 
-	public void setCardValue(String cardValue) {
-		this.cardValue = cardValue;
-	}
+	public String getCardValue() { return cardValue; }
 
-	public String getCardSuit() {
-		return cardSuit;
-	}
+	public void setCardValue(String cardValue) { this.cardValue = cardValue; }
 
-	public void setCardSuit(String cardSuit) {
-		this.cardSuit = cardSuit;
-	}
+	public String getCardSuit() { return cardSuit; }
+
+	public void setCardSuit(String cardSuit) { this.cardSuit = cardSuit; }
 
 }
--- Assignment1/poker/Hand.java
+++ Assignment1/poker/Hand.java
@@ -1,42 +1,78 @@
 package poker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Hand {
-	private Card[] cards;
+	private ArrayList<Card> cards;
 	
 	public Hand() {
-		cards = new Card[5];
+		cards = new ArrayList<Card>();
 	}
 
 	public Hand(Card card1, Card card2, Card card3, Card card4, Card card5) {
-		cards = new Card[5];
-		cards[0] = card1;
-		cards[1] = card2;
-		cards[2] = card3;
-		cards[3] = card4;
-		cards[4] = card5;
+		cards = new ArrayList<Card>();
+		cards.add(card1);
+		cards.add(card2);
+		cards.add(card3);
+		cards.add(card4);
+		cards.add(card5);
 	}
 
+	/*
+	 * Method to check whether a hand is a royal flush or not.
+	 * (1) Check to see all the cards are the same suit
+	 * (2) Check to see all the cards are within the range of Ten - Ace
+	 */
 	public boolean isRoyalFlush() {
-		String tmpSuit = cards[0].getCardSuit();
+		String tmpSuit = cards.get(0).getCardSuit();
 		for (int i = 1; i < 5; i++) {
-			if (!cards[i].getCardSuit().equals(tmpSuit)) {
+			if (!cards.get(i).getCardSuit().equals(tmpSuit)) {
 				return false;
 			}
 		}
+		return cardsWithinRange(8, 14);
+	}
+	
+	/*
+	 * Checks if the list of cards have face values within a certain range.
+	 */
+	private boolean cardsWithinRange(int lowCard, int highCard) {
 		for (int i = 0; i < 5; i++) {
-			String tmpCardVal = cards[i].getCardValue();
-			if (!(tmpCardVal.equals("Ten") || tmpCardVal.equals("Jack") || tmpCardVal.equals("Queen") || tmpCardVal.equals("King") || tmpCardVal.equals("Ace")))
+			int tmpCardVal = cards.get(i).getCardIntValue();
+			if (!(tmpCardVal > lowCard && tmpCardVal < highCard)) 
 				return false;
 		}
 		return true;
 	}
 
 	public void setHand(Card card1, Card card2, Card card3, Card card4, Card card5) {
-		cards[0] = card1;
-		cards[1] = card2;
-		cards[2] = card3;
-		cards[3] = card4;
-		cards[4] = card5;
+		if (cards.size() != 0) {
+			cards.set(0, card1);
+			cards.set(1, card2);
+			cards.set(2, card3);
+			cards.set(3, card4);
+			cards.set(4, card5);
+		}
+		else {
+			cards.add(card1);
+			cards.add(card2);
+			cards.add(card3);
+			cards.add(card4);
+			cards.add(card5);
+		}
+	}
+	
+	/*
+	 * Will probably be used for checking straights/straight flushes.
+	 */
+	private void sortByCardValue(ArrayList<Card> cards) { 
+		Collections.sort(cards, new Comparator<Card>() {
+			public int compare(Card c1, Card c2) {
+				return Integer.valueOf(c1.getCardIntValue()).compareTo(c2.getCardIntValue());
+			}
+		});
 	}
 	
 }
