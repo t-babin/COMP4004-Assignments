869ce32696bbeff37db38bd5ab161d1e77e8becd
Tyler Babin <t.babin9@gmail.com>
2015-09-24 18:05:20 -0400

    refactored game class to pass test
    passes player amount test

--- Assignment1/poker/Game.java
+++ Assignment1/poker/Game.java
@@ -1,13 +1,44 @@
 package poker;
 
-public class Game {
+//import java.util.ArrayList;
 
-	public Game(Player p1, Player p2) {
-		// TODO Auto-generated constructor stub
+public class Game {
+	Player[] players = null;
+
+//	public Game(Player p1, Player p2) {
+//		players = new ArrayList<Player>();
+//		players.add(p1);
+//		players.add(p2);
+//	}
+//	
+//	public Game(Player p1, Player p2, Player p3) {
+//		players = new ArrayList<Player>();
+//		players.add(p1);
+//		players.add(p2);
+//		players.add(p3);
+//	}
+//	
+//	public Game(Player p1, Player p2, Player p3, Player p4) {
+//		players = new ArrayList<Player>();
+//		players.add(p1);
+//		players.add(p2);
+//		players.add(p3);
+//		players.add(p4);
+//	}
+
+	public Game() {
+		setPlayerAmount(0);
 	}
 
 	public int amountOfPlayers() {
-		return 0;
+		return players.length;
+	}
+
+	public void setPlayerAmount(int amount) {
+		players = new Player[amount];
+		for (int i = 0; i < amount; i++)
+			players[i] = new Player();
+
 	}
 
 }
--- Assignment1/tests/game/TestGameInitialization.java
+++ Assignment1/tests/game/TestGameInitialization.java
@@ -5,14 +5,23 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import poker.Game;
-import poker.Player;
+//import poker.Player;
 
 public class TestGameInitialization {
 
 	@Test
 	public void testPlayerAmount() {
-		Game g = new Game(new Player(), new Player());
-		assertEquals(2, g.amountOfPlayers());
+		Game g1 = new Game();
+		g1.setPlayerAmount(2);
+		assertEquals(2, g1.amountOfPlayers());
+		
+		Game g2 = new Game();
+		g2.setPlayerAmount(3);
+		assertEquals(3, g2.amountOfPlayers());
+
+		Game g3 = new Game();
+		g3.setPlayerAmount(4);
+		assertEquals(4, g3.amountOfPlayers());
 	}
 
 }
